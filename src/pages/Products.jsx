6ace8862f93f9a6e63c0b5de5c58a222eff24e8a
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-10 21:03:13 +0530

    feat: move product data to service layer

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
 import ProductCard from '../components/ProductCard'
+import { products } from '../services/ProductService'
+
 function Products() {
-  const products = [
-    {id:1, title:"Laptop",price:50000},
-    {id:2, title:"Phone",price:20000},
-    {id:3, title:"Head Phones",price:5000},
-    {id:4, title:"SmartWatch",price:5000},
-    {id:5, title:"Tv",price:55000}
-  ]
-    
   
   return (
     <div>
---
+++ src/services/productService.js
@@ -0,0 +1,7 @@
+export const products = [
+    {id:1, title:"Laptop",price:50000},
+    {id:2, title:"Phone",price:20000},
+    {id:3, title:"Head Phones",price:5000},
+    {id:4, title:"SmartWatch",price:5000},
+    {id:5, title:"Tv",price:55000}
+  ]
