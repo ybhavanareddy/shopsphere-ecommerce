94fed9479faf9414fe84d8a94820bb62f47d0e38
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-11 14:30:44 +0530

    feat: implement product details page with add to cart functionality

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+
 import { useParams } from 'react-router-dom'
+import { products } from '../services/ProductService';
+
+import { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 
 function ProductDetails() {
+
+    const {addToCart} = useContext(CartContext);
+
     const { id } = useParams();
+
+    const product = products.find(
+      (item) => item.id === Number(id)
+    );
+
   return (
-    <div>
-        <h1>Product Details</h1>
-        <p>Product ID: {id}</p>
+    <div className='p-8 max-w-xl mx-auto'>
+
+        <h1 className='text-3xl font-bold mb-4'>{product.title}</h1>
+
+        <p className='text-gray-600 mb-4'>Price: ${product.price.toFixed(2)}</p>
+
+        <button 
+          onClick={()=> addToCart(product)}
+          className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'
+        >
+          Add to Cart
+        </button>
     </div>
   )
 }
