e57ca554046491f4719a6003c6ad73df6b6c087d
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-12 17:02:29 +0530

    feat: add toast notification for add to cart action

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-
+import { toast } from "react-toastify";
 
 export const CartContext = createContext();
 
@@ -7,16 +7,22 @@ function CartProvider({children}){
     const [cartItems,setCartItems] = useState([]);
 
     function addToCart(product){
-        setCartItems([...cartItems,product])
+        setCartItems([...cartItems,product]);
+
+        toast.success(" Product added to cart");
     }
 
     function removeFromCart(index){
         const updatedCart = cartItems.filter((_,i)=> i !== index);
         setCartItems(updatedCart);
+        toast.error(" Product removed from cart");
+
+
     }
 
     function clearCart(){
         setCartItems([]);
+        toast.info(" Cart cleared");
     }
     
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { BrowserRouter } from 'react-router-dom'
 import CartProvider from './context/CartContext.jsx'
 import AuthProvider from './context/AuthContext.jsx'
@@ -12,6 +15,7 @@ createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <CartProvider>
         <App />
+        <ToastContainer position="top-right" autoClose={1000} />
       </CartProvider>
     </AuthProvider>
     
