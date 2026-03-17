6fd016c04db845afe3eab8ebecd8174ea42c9d4f
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-17 17:56:19 +0530

    feat: persist authentication state using localStorage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,20 +3,31 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 function AuthProvider({children}) {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [username, setUsername] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        localStorage.getItem("isLoggedIn") === "true"
+    );
+    const [username, setUsername] = useState(
+        localStorage.getItem("userName") || ""
+    );
 
     
 
     function login(name){
         setIsLoggedIn(true);
         setUsername(name);
+
+        localStorage.setItem("isLoggedIn", "true");
+        localStorage.setItem("userName",name);
     }
 
     function logout(){
         setIsLoggedIn(false);    
         setUsername("");
+
+        localStorage.removeItem("isLoggedIn");
+        localStorage.removeItem("userName");
     }
+    
 
     return (
         <AuthContext.Provider value={{isLoggedIn,login,logout, username}}>
